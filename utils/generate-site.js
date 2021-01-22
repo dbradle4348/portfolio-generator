8e051006475dad95d29451dadde92f854d2587d5
King Julian <daniel.bradley09@gmail.com>
2021-01-22 14:34:23 -0700

    final

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,41 +1,37 @@
 const fs = require('fs');
-const { resolve } = require('path');
 
+// writing files
 const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', fileContent, err => {
-            // if there's an error, reject the Promise and send the error to the promise's catch method
-           if (err) {
-               reject(err);
-               //return out of the funtion here to make suer the Promise doesn't accidentally execute the resolve() function as well
-               return;
-           }
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-           // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-resolve({
-    ok: true,
-    message: 'File created!'
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
   });
-});
-});
 };
 
-const copyFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.copyFile('./dist/style.css', fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-resolve({
-    ok: true,
-    message: 'File copied!'
-});
-});
-});
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
 };
 
-module.exports = {
-    writeFIle: writeFile,
-    copyFile: copyFile
-};
+module.exports = { writeFile, copyFile };
